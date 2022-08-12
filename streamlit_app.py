22bc3f1825dc21fca9edac6bd68ff553a59606fc
Itradeutrade <111111995+Itradeutrade007@users.noreply.github.com>
2022-08-12 14:19:52 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,4 +3,4 @@ import streamlit
 streamlit.title('My Parents Healthy Diner')
 streamlit.header('Breakfast Menu')
 streamlit.text('Omega3 and Blue Berry Omlet')
-Stremlit.text('kale Spinach & Rocket Smoothie')
+streamlit.text('kale Spinach & Rocket Smoothie')
